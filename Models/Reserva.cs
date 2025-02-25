a4b57bcbe9bba5b1e6d824440c2a223c27ec5d4c
Anderson Coelho Goulart <acgoulartmail@gmail.com>
2025-02-25 14:42:32 -0300

    create ObterQuantidadedeHospedes method

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,18 +15,12 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-
             if (Hospedes.Capacity >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-
                 throw new Exception("Capacidade menor que o número de hóspedes");
             }
         }
@@ -40,7 +34,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
